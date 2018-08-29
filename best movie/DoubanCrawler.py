aabfecc85cc2c742342b9108d26b2ac1a42388a1
t0li <tianxing.li.cn@gmail.com>
2018-08-29 09:39:36 +0200

    add task

--- best movie/DoubanCrawler.py
+++ best movie/DoubanCrawler.py
@@ -2,3 +2,14 @@ import expanddouban as ed
 from bs4 import BeautifulSoup as bs
 import csv
 
+#任务1:获取每个地区、每个类型页面的URL
+
+#任务2: 获取电影页面 HTML
+
+#任务3: 定义电影类
+
+#任务4: 获得豆瓣电影的信息
+
+#任务5: 构造电影信息数据表
+
+#任务6: 统计电影数据
