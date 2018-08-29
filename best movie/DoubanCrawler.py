5aa70912ff594214a9bee11faa817fedb82a083a
t0li <tianxing.li.cn@gmail.com>
2018-08-29 12:48:25 +0200

    finish T4

--- best movie/DoubanCrawler.py
+++ best movie/DoubanCrawler.py
@@ -9,42 +9,64 @@ return a string corresponding to the URL of douban movie lists given category an
 """
 
 def getMovieUrl(category, location):
-	url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,{},{}".format(category, location)
-	return url
+    url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,{},{}".format(category, location)
+    return url
 
 #test
 #print(getMovieUrl('剧情','美国'))
 
 #任务2: 获取电影页面 HTML
 
-url = getMovieUrl('剧情','美国')
+#url = getMovieUrl('剧情','美国')
 
-html = ed.getHtml(url, loadmore = "true")
+#html = ed.getHtml(url, loadmore = "true")
 
 #print(html)#test
 
 #任务3: 定义电影类
-class Movie(object):
-	"""name, rate, location, category, info_link, cover_link for Movie"""
-	def __init__(self, name, rate, location, category, info_link, cover_link):
-		super(Movie, self).__init__()
-		self.name, rate, location, category, info_link, cover_link
-		
-
-
-
-
-
-
-
-
-
-
-
-
+class Movie:
+    """name, rate, location, category, info_link, cover_link for Movie"""
+    def __init__(self, name, rate, location, category, info_link, cover_link):
+        self.name = name
+        self.rate = rate
+        self.location = location
+        self.category = category
+        self.info_link = info_link
+        self.cover_link = cover_link
+    def listing(self):
+    	return [self.name, self.rate, self.location, self.category, self.info_link, self.cover_link]
 
 
 #任务4: 获得豆瓣电影的信息
+"""
+return a list of Movie objects with the given category and location.
+"""
+def getMovies(category, location):
+	#get url
+    url = getMovieUrl(category, location)
+    #get html
+    html = ed.getHtml(url, loadmore = 'true')
+    #analysis
+    soup = bs(html, 'html.parser')
+    movies = soup.find(class_="list-wp")
+    moviesInfo = []
+    #print(movies.find_all('a'))
+    
+    for i in movies.find_all('a'):
+    	name = i.find(class_ = 'title').string
+    	rate = i.find(class_ = 'rate').string
+    	info_link = i.get('href')
+    	cover_link = i.find('img').get('src')
+    	print(name, rate, info_link, cover_link)
+        
+    	moviesInfo.append(Movie(name, rate, category, location, info_link, cover_link).listing())
+
+    for i in moviesInfo:
+    	print(i)
+    return moviesInfo
+
+#print(list(getMovies('剧情','美国')))
+getMovies('剧情','美国')
 
 #任务5: 构造电影信息数据表
 
