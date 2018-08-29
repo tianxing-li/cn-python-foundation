24e46bdd9f44163ab7d0bd869310357707f82bd2
t0li <tianxing.li.cn@gmail.com>
2018-08-29 09:55:04 +0200

    finish T1

--- best movie/DoubanCrawler.py
+++ best movie/DoubanCrawler.py
@@ -4,6 +4,17 @@ import csv
 
 #任务1:获取每个地区、每个类型页面的URL
 
+"""
+return a string corresponding to the URL of douban movie lists given category and location.
+"""
+
+def getMovieUrl(category, location):
+	url = "https://movie.douban.com/tag/#/?sort=S&range=9,10&tags=电影,{},{}".format(category, location)
+	return url
+
+#test
+#print(getMovieUrl('剧情','美国'))
+
 #任务2: 获取电影页面 HTML
 
 #任务3: 定义电影类
